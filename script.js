b5cee1b2d35e51c910de7148401d094ce4458a46
uoiuo <79719306+uoiuo@users.noreply.github.com>
2023-04-13 17:40:07 -0600

    First Commit
    Uploaded files to github for sharing.

---
+++ script.js
@@ -0,0 +1,63 @@
+		var slideIndex = 1;
+		  showSlides(slideIndex);
+		  
+		function plusSlides(n) {  
+			showSlides(slideIndex += n);
+		  }
+		  
+		function showSlides(n) {
+			var i;
+			var slides = document.getElementsByClassName("mySlides");
+			if (n > slides.length) {slideIndex = 1}
+			if (n < 1) {slideIndex = slides.length}
+			for (i = 0; i < slides.length; i++) {
+			  slides[i].style.display = "none";
+			}
+			slides[slideIndex-1].style.display = "block";
+		  }    
+	  
+		var xDown = null;
+		var yDown = null;
+		document.addEventListener('touchstart', handleTouchStart, false);
+		document.addEventListener('touchmove', handleTouchMove, false);
+		document.addEventListener('touchend', handleTouchEnd, false);
+
+		function handleTouchStart(event) {
+		  xDown = event.touches[0].clientX;
+		  yDown = event.touches[0].clientY;
+		};
+
+		function handleTouchMove(event) {
+		  if (!xDown || !yDown) {
+			return;
+		  }
+
+		  var xUp = event.touches[0].clientX;
+		  var yUp = event.touches[0].clientY;
+
+		  var xDiff = xDown - xUp;
+		  var yDiff = yDown - yUp;
+
+		  if (Math.abs(xDiff) > Math.abs(yDiff)) { 
+			if (xDiff > 0) {
+			  plusSlides(1);
+			} else {
+			  plusSlides(-1);
+			}
+		  }
+		  
+		  xDown = null;
+		  yDown = null;
+		};
+
+		function handleTouchEnd(event) {
+		  xDown = null;
+		  yDown = null;
+		};
+		function toggleFullScreen(img) {
+			if (!document.fullscreenElement) {
+				img.requestFullscreen();
+			} else {
+				document.exitFullscreen();
+			}
+		}
